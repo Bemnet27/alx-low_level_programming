61a24e0bf8832c1a9657599b619553bebe9c70e6
bemnet <bemnetabebe1@gmail.com>
2023-11-20 11:06:00 -0800

    edited the return of malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,19 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	if (size == 0 || ptr == NULL)
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 	else
 	{
-		ptr = malloc(size * sizeof(char));
 
 		for (i = 0; i <= size; i++)
 		{
