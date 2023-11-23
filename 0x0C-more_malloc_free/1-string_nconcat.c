488f7399518968bce244c51d28aca1af766ea7e2
bemnet <bemnetabebe1@gmail.com>
2023-11-23 10:57:24 -0800

    string concating

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strlen - length of string
+ * @str: string
+ *
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * string_nconcat - concats twoo string until n
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bytes
+ *
+ * Return: the concated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *new;
+	unsigned int len1;
+	unsigned int i;
+	unsigned int len2, j;
+
+	if (s1 == 0)
+	{
+		return (NULL);
+	}
+	if (s2 == 0)
+	{
+		return (NULL);
+	}
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	new = malloc((len1 + n + 1) * sizeof(char));
+	if (new == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= len1; i++)
+	{
+		new[i] = s1[i];
+	}
+	if (n < len2)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			new[len1 + j] = s2[j];
+		}
+	}
+	else
+	{
+		for (j = 0; j <= len2; j++)
+		{
+			new[len1 + j] = s2[j];
+		}
+	}
+		return (new);
+}
