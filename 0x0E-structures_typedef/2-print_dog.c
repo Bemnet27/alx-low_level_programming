a1ffcdd50a9fe981004d1ba55dc5c610fefd2396
bemnet <bemnetabebe1@gmail.com>
2023-11-27 14:02:48 -0800

    edited the null statement for d

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,25 +10,24 @@ void print_dog(struct dog *d)
 {
 	char *empty = "";
 
-	if (d == NULL)
+	if (d != NULL)
 	{
-		printf("%s\n", empty);
-	}
-	if (d->name == NULL)
-	{
-		printf("Name: (nil)\n");
-	}
-	else
-	{
-		printf("Name: %s\n", (*d).name);
-	}
-		printf("Age: %f\n", (*d).age);
-	if (d->owner == NULL)
-	{
-		printf("Owner: (nil)\n");
-	}
-	else
-	{
+			if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+			else
+		{
+			printf("Name: %s\n", (*d).name);
+		}
+			printf("Age: %f\n", (*d).age);
+			if (d->owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+			else
+		{
 		printf("Owner: %s\n", (*d).owner);
+		}
 	}
 }
