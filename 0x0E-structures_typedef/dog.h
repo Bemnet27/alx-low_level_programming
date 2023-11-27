c26e70ebe94ca82e522ba25f2aba8df01291afce
bemnet <bemnetabebe1@gmail.com>
2023-11-27 13:30:26 -0800

    betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,12 @@
  *
  * Description: Longer description
  */
-struct dog{
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
-#endif
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+#endif
