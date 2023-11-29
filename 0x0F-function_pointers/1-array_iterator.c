d1c14372542f761f2b9da9c7a4964a58fd49e340
bemnet <bemnetabebe1@gmail.com>
2023-11-29 14:55:08 -0800

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes given paramaters
+ * @array: array
+ * @action: a pointer to a function
+ * @size: size of an array
+ *
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
