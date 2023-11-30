868e544e97aac5b1645645278df399e192636d17
bemnet <bemnetabebe1@gmail.com>
2023-11-29 16:24:36 -0800

    int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,38 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @size: size of the array
+ * @array: the array
+ * @cmp: pointer to a function
+ *
+ * Return: index of the array for success
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, a;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (cmp == 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		a = cmp(array[i]);
+	}
+		if (a)
+		{
+			return (i);
+		}
+		else
+		{
+			return (-1);
+		}
+}
