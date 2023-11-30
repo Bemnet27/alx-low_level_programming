45813f6580eb424e7258c94842034cc1d5e59969
bemnet <bemnetabebe1@gmail.com>
2023-11-30 11:30:01 -0800

    edited the conditions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,10 +19,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 	{
-		return;
+		separator = "";
 	}
 	if (n == 0)
 	{
+		printf("\n");
 		return;
 	}
 	for (i = 0; i < n - 1; i++)
